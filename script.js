d9c2e079794eccc59368d7ac2782ff7617808f2c
gopi-j2 <gopi.j2@unisys.com>
2024-06-03 17:40:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const iframe = document.querySelector('iframe');
+  const requestFullScreen = (element) => {
+    const requestMethods = ['requestFullscreen', 'mozRequestFullScreen', 'webkitRequestFullscreen', 'msRequestFullscreen'];
+    const hasFullScreenApi = document.fullscreenEnabled || document.webkitFullscreenEnabled || document.mozFullScreenEnabled || document.msFullscreenEnabled;
+    const requestMethod = requestMethods.find(method => element[method]);
+
+    if (hasFullScreenApi && requestMethod) {
+      element[requestMethod]().catch(err => console.log(err));
+    }
+  }
+
+  document.addEventListener('click', () => {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop,
+        offsetTop = iframe.offsetTop;
+
+    requestFullScreen(iframe);
+  })
+})
